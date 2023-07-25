2c7759a558ce1486b151da7fb85c6b208dbd5cd6
CRXAhsan <ahsantariq669@gmail.com>
2023-07-26 00:26:28 +0500

    array practice

--- array/practice.cpp
+++ array/practice.cpp
@@ -25,12 +25,12 @@ int elementApperaOnceInArray(int arr[], int size)
 
 // Write a program to print all the LEADERS in the array. An element is a leader if it is greater than all the elements to its right side. And the rightmost element is always a leader.
 
-void printAllLeaderElements(int *arr , int size)
+void printAllLeaderElements(int *arr, int size)
 {
     int count;
     for (int i = 0; i < size; i++)
     {
-        count=0;
+        count = 0;
         for (int j = i; j < size; j++)
         {
             if (arr[i] < arr[j])
@@ -38,20 +38,51 @@ void printAllLeaderElements(int *arr , int size)
                 if (arr[i] != arr[j])
                 {
                     count++;
-                }                
+                }
             }
-            
         }
 
         if (count == 0)
         {
-            cout<<arr[i]<<" , ";
+            cout << arr[i] << " , ";
         }
     }
-    
 }
 
+// Find Subarray with given sum | Set 1 (Non-negative Numbers)
 
+void subArrayOfGivenSum(int *arr, int size)
+{
+    int sum, totalSum, start, end, count = 0;
+    cout << "Enter sum you want array size against that : ";
+    cin >> sum;
+    for (int i = 0; i < size; i++)
+    {
+
+        totalSum = 0;
+
+        for (int j = i; j < size; j++)
+        {
+            totalSum += arr[j];
+            if (sum == totalSum)
+            {
+                start = arr[i];
+                end = arr[j];
+                cout << "Sum found between index " << i << " & " << j << "\nThe elements are: \n";
+                for (int k = i; k <= j; k++)
+                {
+                    cout << arr[k] << " , ";
+                }
+                cout<<"\n";
+                count++;
+            }
+        }
+    }
+    if (count == 0)
+    {
+        cout << "No any sub array found for the given sum\n";
+    }
+}
 
 int main()
 {
@@ -65,9 +96,10 @@ int main()
         cin >> arr[i];
     }
 
-    //temp = elementApperaOnceInArray(arr, size);
-    //cout << "Value " << temp << " is present one time in the whole array\n";
+    // temp = elementApperaOnceInArray(arr, size);
+    // cout << "Value " << temp << " is present one time in the whole array\n";
 
+    // printAllLeaderElements(arr, size);
 
-    printAllLeaderElements(arr,size);
+    subArrayOfGivenSum(arr, size);
 }
